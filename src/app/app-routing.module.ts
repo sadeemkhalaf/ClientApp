e5611378e0d6cea7a339195999ff978a893f6c6f
Sadeem Khalaf <sadeem@capella.io>
2019-07-01 00:32:59 +0300

    added new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CandidatesSummaryComponent } from './candidates-summary/candidates-summary.component';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
 
 const routes: Routes = [{path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-{ path: 'dashboard', component: CandidatesSummaryComponent}
+{ path: 'dashboard', component: MainDashboardComponent},
+{ path: 'dashboard/applicants', component: CandidatesSummaryComponent}
 ];
 
 @NgModule({
---
+++ src/app/dashboardComponents/applicants-inbox/applicants-inbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-applicants-inbox',
+  templateUrl: './applicants-inbox.component.html',
+  styleUrls: ['./applicants-inbox.component.css']
+})
+export class ApplicantsInboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
