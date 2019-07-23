0a8d588c7de4c9d03dbeca472cd14f0fbb785d71
sadeemcapella <sadeem@capella.io>
2019-07-23 19:15:52 +0300

    router module updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { CandidateDetailsComponent } from './candidate-details/candidate-details
 const routes: Routes = [{path: '', redirectTo: '/dashboard', pathMatch: 'full'},
 { path: 'dashboard', component: MainDashboardComponent},
 { path: 'dashboard/applicants', component: CandidatesSummaryComponent},
+{ path: 'dashboard/applicants/add-new', component: CandidatesFormComponent},
 { path: 'dashboard/apply', component: CandidatesFormComponent},
 { path: 'dashboard/details/:id', component: CandidateDetailsComponent}
 ];
